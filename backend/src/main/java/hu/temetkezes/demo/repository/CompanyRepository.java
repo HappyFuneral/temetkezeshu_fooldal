b29cbf08fdeee5992565dc66f5656f65eb0bef47
milan <milan.maroevich@gmail.com>
2024-05-22 01:06:52 +0200

    init

--- backend/src/main/java/hu/temetkezes/demo/repository/CompanyRepository.java
+++ backend/src/main/java/hu/temetkezes/demo/repository/CompanyRepository.java
@@ -4,7 +4,7 @@ import hu.temetkezes.demo.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CompanyRepository extends JpaRepository<Company,Long> {
     Company findByShortName(String shortName);
 }
--- backend/src/main/java/hu/temetkezes/demo/repository/OfficeRepository.java
+++ backend/src/main/java/hu/temetkezes/demo/repository/OfficeRepository.java
@@ -4,6 +4,5 @@ import hu.temetkezes.demo.models.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OfficeRepository extends JpaRepository<Office,Long> {
 }

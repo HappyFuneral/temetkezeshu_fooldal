fc9acae3d6e4f8b5b48202a7306b75c25e0dc8a6
Milan Maroevich <milan.maroevich@gmail.com>
2024-05-30 01:07:55 +0200

    corsconfig

--- backend/src/main/java/hu/temetkezes/demo/config/CorsConfiguration.java
+++ backend/src/main/java/hu/temetkezes/demo/config/CorsConfiguration.java
@@ -9,8 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    private final String frontend = "http://localhost:4000";
-    private final String backend = "http://localhost:8080";
+    private final String frontend = "http://react-app:4000";
+    private final String backend = "http://spring-boot-app:8080";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {

4ecd1a7b4275655cc8d599e98f3046d791da348d
happy <happyfuneral40@gmail.com>
2024-05-30 10:15:39 +0200

    corsconfig

--- backend/src/main/java/hu/temetkezes/demo/config/CorsConfiguration.java
+++ backend/src/main/java/hu/temetkezes/demo/config/CorsConfiguration.java
@@ -16,6 +16,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins(backend, frontend);
-        ;
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("http://localhost:8080", "http://localhost:4000");
+
     }
 }

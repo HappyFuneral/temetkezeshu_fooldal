b6611bc1d06ad45a68241a1ce302681e26a7f679
happy <happyfuneral40@gmail.com>
2024-07-04 15:17:58 +0200

    corsconfig

--- frontend/src/components/CodeMap.jsx
+++ frontend/src/components/CodeMap.jsx
@@ -1,41 +1,10 @@
-import React from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import React, {useState} from "react";
+import {GoogleMap, useLoadScript, MarkerF, InfoWindow} from "@react-google-maps/api";
 import Office from "./Office.jsx";
 import mapicon from "../img/mapicon.png";
 
 const MapComponent = ({ selectedLocation, offices,zoom }) => {
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: "AIzaSyDWtnOf9mfiuz8Y8FZU-UxeJCCVOe8KLFU",
-    });
-    const mapRef = React.useRef();
-    const onMapLoad = React.useCallback((map) => {
-        mapRef.current = map;
-    }, []);
-    if (loadError) return "Error";
-    if (!isLoaded) return "Maps";
 
-    return (
-        <div className="h-flexible">
-            <GoogleMap
-                mapContainerStyle={{
-                    height: "100%",
-                }}
-                center={selectedLocation}
-                zoom={zoom}
-                onLoad={onMapLoad}
-            >
-                {offices.map((office) => {
-                    return (
-                        <MarkerF
-                            position={{lat: office.latitude, lng: office.longitude}}
-                            icon={mapicon}
-                        />
-                    );
-                })}
-
-            </GoogleMap>
-        </div>
-    );
 };
 
 export default MapComponent;
--- frontend/src/components/Regions.jsx
+++ frontend/src/components/Regions.jsx
@@ -4,8 +4,8 @@ import React, {useEffect, useRef, useState} from "react";
 import axiosClient from "../axios-client.js";
 import {Link, useParams} from "react-router-dom";
 
-import {Carousel} from "flowbite-react";
-import MapComponent from "./CodeMap.jsx";
+import {GoogleMap, InfoWindow, MarkerF, useLoadScript} from "@react-google-maps/api";
+import mapicon from "../img/mapicon.png";
 
 
 export const RegionMap = () => {
@@ -18,15 +18,6 @@ export const RegionMap = () => {
         lng:  19.056156586745704
     });
     const [zoom,setZoom] = useState(13)
-    const [loaded, setLoaded] = useState(false)
-
-    useEffect(() => {
-
-        if(!loaded)
-            getRegions()
-
-    }, [offices]);
-
 
 
 
@@ -50,6 +41,43 @@ export const RegionMap = () => {
         setLoaded(true)
     }
 
+
+    useEffect(() => {
+
+        if(!loaded)
+            getRegions()
+
+    }, [offices]);
+
+
+
+
+    const [loaded, setLoaded] = useState(false)
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: "AIzaSyDWtnOf9mfiuz8Y8FZU-UxeJCCVOe8KLFU",
+    });
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+    const [selectedMarker, setSelectedMarker] = useState()
+
+
+
+    if (loadError) return "Error";
+    if (!isLoaded) return "Maps";
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
             <div className="w-full bg-black ">
@@ -72,14 +100,15 @@ export const RegionMap = () => {
                 </div>
             </div>
             <div className="grid lg:grid-cols-2 lg:gap-2 grid-cols-1">
-
                 <div className="overflow-y-scroll overflow-visible w-full antialiased h-flexible">
                     {offices.map((office) => {
                         return (
                             <div onClick={() =>
                             {
+
                                 setSelectedLocation({lat: office.latitude, lng: office.longitude})
                                 setZoom(17)
+                                setSelectedMarker(office)
                             }
                             } >
                                 <Office office={office} key={office.id}/>
@@ -88,7 +117,40 @@ export const RegionMap = () => {
                                   );
                     })}
                 </div>
-                <MapComponent selectedLocation={selectedLocation} zoom={zoom} offices={offices}/>
+                <div className="h-flexible">
+                    <GoogleMap
+                        mapContainerStyle={{
+                            height: "100%",
+                        }}
+                        center={selectedLocation}
+                        zoom={zoom}
+                        onLoad={onMapLoad}
+                    >
+                        {offices.map((office) => {
+                            return (
+                                <MarkerF onClick={() => {
+                                    setSelectedMarker(office)
+                                    setSelectedLocation ({lat: office.latitude, lng: office.longitude})
+                                    setZoom(17)
+                                }}
+                                         position={{lat: office.latitude, lng: office.longitude}}
+                                         icon={mapicon}
+                                />
+                            );
+                        })}
+                        {selectedMarker &&
+                            <InfoWindow position={{lat: selectedMarker.latitude+0.000252, lng: selectedMarker.longitude}}
+                                        onCloseClick={() => {
+                                            setSelectedMarker()
+                                        }}>
+                                <>
+                                    <h1>{selectedMarker.name}</h1>
+                                    <p>{selectedMarker.location}</p>
+                                </>
+                            </InfoWindow>
+                        }
+                    </GoogleMap>
+                </div>
             </div>
         </div>
 
